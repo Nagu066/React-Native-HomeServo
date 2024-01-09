d54bb165aae169392ac1cad42b0d8476665053b3
Nagu066 <nagarajmoger11@gmail.com>
2024-01-09 11:41:18 +0530

    Home Service app

---
+++ App/Screens/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Header from './Header'
+import Slider from './Slider'
+
+export default function HomeScreen() {
+  return (
+    <View>
+      {/* Header */}
+      <Header/>
+      <View style={{padding:20}}>
+        {/* Slider */}
+        <Slider/>
+      </View>
+      
+    </View>
+  )
+}
---
+++ App/Utils/GlobalApi.js
@@ -0,0 +1,23 @@
+import { request, gql } from 'graphql-request'
+
+const MASTER_URL = 'https://api-ap-south-1.hygraph.com/v2/clr4c6zajlpvj01wal71q0qxh/master';
+const getSlider=async()=>{
+    const query = gql`
+    query GetSlider {
+        sliders {
+          id
+          name
+          image {
+            url
+          }
+        }
+      }
+`
+const result = await request(MASTER_URL, query);
+return result;
+}
+
+export default {
+    getSlider
+}
+
