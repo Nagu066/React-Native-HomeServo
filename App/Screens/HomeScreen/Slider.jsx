ee10bc5ac633733d2c701a9b50d37e1097534d8c
Nagu066 <nagarajmoger11@gmail.com>
2024-01-09 22:28:04 +0530

    Home Screen Categories

--- App/Screens/HomeScreen/Slider.jsx
+++ App/Screens/HomeScreen/Slider.jsx
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet, FlatList, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import GlobalApi from '../../Utils/GlobalApi'
+import Heading from '../../Components/Heading';
 
 export default function Slider() {
     const [slider, setSlider] = useState([]);
@@ -15,19 +16,19 @@ export default function Slider() {
     }
   return (
     <View>
-      <Text style={styles.heading}>Offers For You</Text>
-      <FlatList
-        data={slider}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        renderItem={({item, index})=>(
-            <View style={{marginRight:20}}>
-                <Image source={{uri:item?.image?.url}}
-                style={styles.sliderImage}
-                />
-            </View>
-        )}
-      />
+        <Heading text={'Offers For You'}/> 
+        <FlatList
+            data={slider}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({item, index})=>(
+                <View style={{marginRight:20}}>
+                    <Image source={{uri:item?.image?.url}}
+                    style={styles.sliderImage}
+                    />
+                </View>
+            )}
+        />
     </View>
   )
 }
